b8f0ebc57d1c4880a9e9d0e4aeedcd70815575ea
unknown <hadifsakr@gmail.com>
2023-07-05 02:14:57 +0300

    first commit

---
+++ Code_indexer.py
@@ -0,0 +1,699 @@
+class PorterStemmer:
+
+    def __init__(self):
+        """The main part of the stemming algorithm starts here.
+        b is a buffer holding a word to be stemmed. The letters are in b[k0],
+        b[k0+1] ... ending at b[k]. In fact k0 = 0 in this demo program. k is
+        readjusted downwards as the stemming progresses. Zero termination is
+        not in fact used in the algorithm.
+
+        Note that only lower case sequences are stemmed. Forcing to lower case
+        should be done before stem(...) is called.
+        """
+
+        self.b = ""  # buffer for word to be stemmed
+        self.k = 0
+        self.k0 = 0
+        self.j = 0   # j is a general offset into the string
+
+    def cons(self, i):
+        """cons(i) is TRUE <=> b[i] is a consonant."""
+        if self.b[i] == 'a' or self.b[i] == 'e' or self.b[i] == 'i' or self.b[i] == 'o' or self.b[i] == 'u':
+            return 0
+        if self.b[i] == 'y':
+            if i == self.k0:
+                return 1
+            else:
+                return (not self.cons(i - 1))
+        return 1
+
+    def m(self):
+        """m() measures the number of consonant sequences between k0 and j.
+        if c is a consonant sequence and v a vowel sequence, and <..>
+        indicates arbitrary presence,
+
+           <c><v>       gives 0
+           <c>vc<v>     gives 1
+           <c>vcvc<v>   gives 2
+           <c>vcvcvc<v> gives 3
+           ....
+        """
+        n = 0
+        i = self.k0
+        while 1:
+            if i > self.j:
+                return n
+            if not self.cons(i):
+                break
+            i = i + 1
+        i = i + 1
+        while 1:
+            while 1:
+                if i > self.j:
+                    return n
+                if self.cons(i):
+                    break
+                i = i + 1
+            i = i + 1
+            n = n + 1
+            while 1:
+                if i > self.j:
+                    return n
+                if not self.cons(i):
+                    break
+                i = i + 1
+            i = i + 1
+
+    def vowelinstem(self):
+        """vowelinstem() is TRUE <=> k0,...j contains a vowel"""
+        for i in range(self.k0, self.j + 1):
+            if not self.cons(i):
+                return 1
+        return 0
+
+    def doublec(self, j):
+        """doublec(j) is TRUE <=> j,(j-1) contain a double consonant."""
+        if j < (self.k0 + 1):
+            return 0
+        if (self.b[j] != self.b[j-1]):
+            return 0
+        return self.cons(j)
+
+    def cvc(self, i):
+        """cvc(i) is TRUE <=> i-2,i-1,i has the form consonant - vowel - consonant
+        and also if the second c is not w,x or y. this is used when trying to
+        restore an e at the end of a short  e.g.
+
+           cav(e), lov(e), hop(e), crim(e), but
+           snow, box, tray.
+        """
+        if i < (self.k0 + 2) or not self.cons(i) or self.cons(i-1) or not self.cons(i-2):
+            return 0
+        ch = self.b[i]
+        if ch == 'w' or ch == 'x' or ch == 'y':
+            return 0
+        return 1
+
+    def ends(self, s):
+        """ends(s) is TRUE <=> k0,...k ends with the string s."""
+        length = len(s)
+        if s[length - 1] != self.b[self.k]: # tiny speed-up
+            return 0
+        if length > (self.k - self.k0 + 1):
+            return 0
+        if self.b[self.k-length+1:self.k+1] != s:
+            return 0
+        self.j = self.k - length
+        return 1
+
+    def setto(self, s):
+        """setto(s) sets (j+1),...k to the characters in the string s, readjusting k."""
+        length = len(s)
+        self.b = self.b[:self.j+1] + s + self.b[self.j+length+1:]
+        self.k = self.j + length
+
+    def r(self, s):
+        """r(s) is used further down."""
+        if self.m() > 0:
+            self.setto(s)
+
+    def step1ab(self):
+        """step1ab() gets rid of plurals and -ed or -ing. e.g.
+
+           caresses  ->  caress
+           ponies    ->  poni
+           ties      ->  ti
+           caress    ->  caress
+           cats      ->  cat
+
+           feed      ->  feed
+           agreed    ->  agree
+           disabled  ->  disable
+
+           matting   ->  mat
+           mating    ->  mate
+           meeting   ->  meet
+           milling   ->  mill
+           messing   ->  mess
+
+           meetings  ->  meet
+        """
+        if self.b[self.k] == 's':
+            if self.ends("sses"):
+                self.k = self.k - 2
+            elif self.ends("ies"):
+                self.setto("i")
+            elif self.b[self.k - 1] != 's':
+                self.k = self.k - 1
+        if self.ends("eed"):
+            if self.m() > 0:
+                self.k = self.k - 1
+        elif (self.ends("ed") or self.ends("ing")) and self.vowelinstem():
+            self.k = self.j
+            if self.ends("at"):   self.setto("ate")
+            elif self.ends("bl"): self.setto("ble")
+            elif self.ends("iz"): self.setto("ize")
+            elif self.doublec(self.k):
+                self.k = self.k - 1
+                ch = self.b[self.k]
+                if ch == 'l' or ch == 's' or ch == 'z':
+                    self.k = self.k + 1
+            elif (self.m() == 1 and self.cvc(self.k)):
+                self.setto("e")
+
+    def step1c(self):
+        """step1c() turns terminal y to i when there is another vowel in the stem."""
+        if (self.ends("y") and self.vowelinstem()):
+            self.b = self.b[:self.k] + 'i' + self.b[self.k+1:]
+
+    def step2(self):
+        """step2() maps double suffices to single ones.
+        so -ization ( = -ize plus -ation) maps to -ize etc. note that the
+        string before the suffix must give m() > 0.
+        """
+        if self.b[self.k - 1] == 'a':
+            if self.ends("ational"):   self.r("ate")
+            elif self.ends("tional"):  self.r("tion")
+        elif self.b[self.k - 1] == 'c':
+            if self.ends("enci"):      self.r("ence")
+            elif self.ends("anci"):    self.r("ance")
+        elif self.b[self.k - 1] == 'e':
+            if self.ends("izer"):      self.r("ize")
+        elif self.b[self.k - 1] == 'l':
+            if self.ends("bli"):       self.r("ble") # --DEPARTURE--
+            # To match the published algorithm, replace this phrase with
+            #   if self.ends("abli"):      self.r("able")
+            elif self.ends("alli"):    self.r("al")
+            elif self.ends("entli"):   self.r("ent")
+            elif self.ends("eli"):     self.r("e")
+            elif self.ends("ousli"):   self.r("ous")
+        elif self.b[self.k - 1] == 'o':
+            if self.ends("ization"):   self.r("ize")
+            elif self.ends("ation"):   self.r("ate")
+            elif self.ends("ator"):    self.r("ate")
+        elif self.b[self.k - 1] == 's':
+            if self.ends("alism"):     self.r("al")
+            elif self.ends("iveness"): self.r("ive")
+            elif self.ends("fulness"): self.r("ful")
+            elif self.ends("ousness"): self.r("ous")
+        elif self.b[self.k - 1] == 't':
+            if self.ends("aliti"):     self.r("al")
+            elif self.ends("iviti"):   self.r("ive")
+            elif self.ends("biliti"):  self.r("ble")
+        elif self.b[self.k - 1] == 'g': # --DEPARTURE--
+            if self.ends("logi"):      self.r("log")
+        # To match the published algorithm, delete this phrase
+
+    def step3(self):
+        """step3() dels with -ic-, -full, -ness etc. similar strategy to step2."""
+        if self.b[self.k] == 'e':
+            if self.ends("icate"):     self.r("ic")
+            elif self.ends("ative"):   self.r("")
+            elif self.ends("alize"):   self.r("al")
+        elif self.b[self.k] == 'i':
+            if self.ends("iciti"):     self.r("ic")
+        elif self.b[self.k] == 'l':
+            if self.ends("ical"):      self.r("ic")
+            elif self.ends("ful"):     self.r("")
+        elif self.b[self.k] == 's':
+            if self.ends("ness"):      self.r("")
+
+    def step4(self):
+        """step4() takes off -ant, -ence etc., in context <c>vcvc<v>."""
+        if self.b[self.k - 1] == 'a':
+            if self.ends("al"): pass
+            else: return
+        elif self.b[self.k - 1] == 'c':
+            if self.ends("ance"): pass
+            elif self.ends("ence"): pass
+            else: return
+        elif self.b[self.k - 1] == 'e':
+            if self.ends("er"): pass
+            else: return
+        elif self.b[self.k - 1] == 'i':
+            if self.ends("ic"): pass
+            else: return
+        elif self.b[self.k - 1] == 'l':
+            if self.ends("able"): pass
+            elif self.ends("ible"): pass
+            else: return
+        elif self.b[self.k - 1] == 'n':
+            if self.ends("ant"): pass
+            elif self.ends("ement"): pass
+            elif self.ends("ment"): pass
+            elif self.ends("ent"): pass
+            else: return
+        elif self.b[self.k - 1] == 'o':
+            if self.ends("ion") and (self.b[self.j] == 's' or self.b[self.j] == 't'): pass
+            elif self.ends("ou"): pass
+            # takes care of -ous
+            else: return
+        elif self.b[self.k - 1] == 's':
+            if self.ends("ism"): pass
+            else: return
+        elif self.b[self.k - 1] == 't':
+            if self.ends("ate"): pass
+            elif self.ends("iti"): pass
+            else: return
+        elif self.b[self.k - 1] == 'u':
+            if self.ends("ous"): pass
+            else: return
+        elif self.b[self.k - 1] == 'v':
+            if self.ends("ive"): pass
+            else: return
+        elif self.b[self.k - 1] == 'z':
+            if self.ends("ize"): pass
+            else: return
+        else:
+            return
+        if self.m() > 1:
+            self.k = self.j
+
+    def step5(self):
+        """step5() removes a final -e if m() > 1, and changes -ll to -l if
+        m() > 1.
+        """
+        self.j = self.k
+        if self.b[self.k] == 'e':
+            a = self.m()
+            if a > 1 or (a == 1 and not self.cvc(self.k-1)):
+                self.k = self.k - 1
+        if self.b[self.k] == 'l' and self.doublec(self.k) and self.m() > 1:
+            self.k = self.k -1
+
+    def stem(self, p, i, j):
+        """In stem(p,i,j), p is a char pointer, and the string to be stemmed
+        is from p[i] to p[j] inclusive. Typically i is zero and j is the
+        offset to the last character of a string, (p[j+1] == '\0'). The
+        stemmer adjusts the characters p[i] ... p[j] and returns the new
+        end-point of the string, k. Stemming never increases word length, so
+        i <= k <= j. To turn the stemmer into a module, declare 'stem' as
+        extern, and delete the remainder of this file.
+        """
+        # copy the parameters into statics
+        self.b = p
+        self.k = j
+        self.k0 = i
+        if self.k <= self.k0 + 1:
+            return self.b # --DEPARTURE--
+
+        # With this line, strings of length 1 or 2 don't go through the
+        # stemming process, although no mention is made of this in the
+        # published algorithm. Remove the line to match the published
+        # algorithm.
+
+        self.step1ab()
+        self.step1c()
+        self.step2()
+        self.step3()
+        self.step4()
+        self.step5()
+        return self.b[self.k0:self.k+1]
+
+#the list of stop words
+stopwords = ['i',
+ 'me',
+ 'my',
+ 'myself',
+ 'we',
+ 'our',
+ 'ours',
+ 'ourselves',
+ 'you',
+ "you're",
+ "you've",
+ "you'll",
+ "you'd",
+ 'your',
+ 'yours',
+ 'yourself',
+ 'yourselves',
+ 'he',
+ 'him',
+ 'his',
+ 'himself',
+ 'she',
+ "she's",
+ 'her',
+ 'hers',
+ 'herself',
+ 'it',
+ "it's",
+ 'its',
+ 'itself',
+ 'they',
+ 'them',
+ 'their',
+ 'theirs',
+ 'themselves',
+ 'what',
+ 'which',
+ 'who',
+ 'whom',
+ 'this',
+ 'that',
+ "that'll",
+ 'these',
+ 'those',
+ 'am',
+ 'is',
+ 'are',
+ 'was',
+ 'were',
+ 'be',
+ 'been',
+ 'being',
+ 'have',
+ 'has',
+ 'had',
+ 'having',
+ 'do',
+ 'does',
+ 'did',
+ 'doing',
+ 'a',
+ 'an',
+ 'the',
+ 'and',
+ 'but',
+ 'if',
+ 'or',
+ 'because',
+ 'as',
+ 'until',
+ 'while',
+ 'of',
+ 'at',
+ 'by',
+ 'for',
+ 'with',
+ 'about',
+ 'against',
+ 'between',
+ 'into',
+ 'through',
+ 'during',
+ 'before',
+ 'after',
+ 'above',
+ 'below',
+ 'to',
+ 'from',
+ 'up',
+ 'down',
+ 'in',
+ 'out',
+ 'on',
+ 'off',
+ 'over',
+ 'under',
+ 'again',
+ 'further',
+ 'then',
+ 'once',
+ 'here',
+ 'there',
+ 'when',
+ 'where',
+ 'why',
+ 'how',
+ 'all',
+ 'any',
+ 'both',
+ 'each',
+ 'few',
+ 'more',
+ 'most',
+ 'other',
+ 'some',
+ 'such',
+ 'no',
+ 'nor',
+ 'not',
+ 'only',
+ 'own',
+ 'same',
+ 'so',
+ 'than',
+ 'too',
+ 'very',
+ 's',
+ 't',
+ 'can',
+ 'will',
+ 'just',
+ 'don',
+ "don't",
+ 'should',
+ "should've",
+ 'now',
+ 'd',
+ 'll',
+ 'm',
+ 'o',
+ 're',
+ 've',
+ 'y',
+ 'ain',
+ 'aren',
+ "aren't",
+ 'couldn',
+ "couldn't",
+ 'didn',
+ "didn't",
+ 'doesn',
+ "doesn't",
+ 'hadn',
+ "hadn't",
+ 'hasn',
+ "hasn't",
+ 'haven',
+ "haven't",
+ 'isn',
+ "isn't",
+ 'ma',
+ 'mightn',
+ "mightn't",
+ 'mustn',
+ "mustn't",
+ 'needn',
+ "needn't",
+ 'shan',
+ "shan't",
+ 'shouldn',
+ "shouldn't",
+ 'wasn',
+ "wasn't",
+ 'weren',
+ "weren't",
+ 'won',
+ "won't",
+ 'wouldn',
+ "wouldn't"]
+
+#  Example code in python programming language demonstrating some of the features of an inverted index.
+#  In this example, we scan a directory containing the corpus of files. (In this case the documents are reports on articles
+#  and authors submitted to the Journal "Communications of the Association for Computing Machinery"  
+#
+#  In this example we see each file being read, tokenized (each word or term is extracted) combined into a sorted 
+#  list of unique terms.  
+#
+#  We also see the creation of a documents dictionary containing each document in sorted form with an index assigned to it.
+#  Each unique term is written out into a terms dictionary in sorted order with an index number assigned for each term.  
+#  From our readings we know that to complete teh inverted index all that we need to do is create a third file that will
+#  coorelate each term with the list of documents that it was extracted from.  We will do that in a later assignment.
+##
+#  We can further develop this example by keeping a reference for each term of the documents that it came from and by 
+#  developing a list of the documents thus creating the term and document dictionaries. 
+#
+#  As you work with this example, think about how you might enhance it to assign a unique index number to each term and to 
+#  each document and how you might create a data structure that links the term index with the document index. 
+
+
+import sys,os,re
+import time
+import math
+from bs4 import BeautifulSoup
+from urllib import request
+
+
+# define global variables used as counters
+tokens = 0
+documents = 0
+terms = 0
+termindex = 0
+docindex = 0 
+#number of stop words
+n_stopwords = [] 
+
+# initialize list variable
+#
+alltokens = []
+alldocs = []
+
+stemmer = PorterStemmer()
+
+#
+# Capture the start time of the routine so that we can determine the total running
+# time required to process the corpus
+#
+t2 = time.localtime()   
+
+N = 0
+df = {}
+tf = {}
+idf = {}
+duplicate = {}
+
+def dfs(soup, documents, url, alldocs, alltokens, tf, df, duplicate, n_stopwords):
+    if len(alldocs) >= 500:
+        return
+
+    if soup.body is None:
+        return
+    body = soup.body.get_text()
+    documents+=1
+    df_temp = {}
+    alldocs.append(url)
+    data = body.replace('\n', '')  
+
+    for token in data.split():
+        # check the word is stop word or not
+        if token in stopwords:
+            n_stopwords.append(token)
+            continue
+            #stem the tokens
+        stem_token = stemmer.stem(token, 0, len(token) - 1)
+        alltokens.append(stem_token)
+        df_temp[stem_token] = 1
+
+        #calculate the term frequency
+        key = stem_token + " " + url
+        if key in tf:
+            tf[key] += 1
+        else:
+            tf[key] = 1
+
+    # calculate document frequency
+    for stem_token in df_temp:
+        if stem_token in df:
+            df[stem_token] += 1
+        else:
+            df[stem_token] = 1
+
+    links = soup.find_all('a')
+    for link in links:
+        url = link.get('href')
+        if url in duplicate:
+            continue
+        duplicate[url] = 1
+        if url is None:
+            continue
+        if "iDownload" in url:
+            continue
+        if "http" not in url:
+            continue
+        try:
+            response = request.urlopen(url, timeout=7)
+        except Exception as e: 
+            continue
+        if response.getcode() == 308:
+            continue
+        if "http://ptp.helpserve.com/rss/index.php?/Knowledgebase/Feed/Index/" in url: 
+            continue
+        if "http://ptp.helpserve.com/rss/index.php?/News/Feed/Index/" in url: 
+            continue
+        if "http://ptp.helpserve.com/rss/index.php?/News/Feed" in url: 
+            continue
+        try:
+            soup = BeautifulSoup(response, "html.parser")
+        except:
+            pass
+        response.close()
+        dfs(soup, documents, url, alldocs, alltokens, tf, df, duplicate, n_stopwords)
+        
+        if len(alldocs) >= 500:
+            break
+        
+
+    
+    return
+
+# set the name of the directory for the corpus
+#
+dirname = "/Users/yamamotok96/CS3308/cacm"
+url = input('Enter URL to crawl (must be in the form http://www.domain.com): ')
+response = request.urlopen(url)
+soup = BeautifulSoup(response, "html.parser")
+response.close()
+N = 0
+dfs(soup, documents, url, alldocs, alltokens, tf, df, duplicate, n_stopwords)
+tokens = len(alltokens)
+N = len(alldocs)
+documents = len(alldocs)
+t3 = time.localtime()
+
+
+#calculate inverse document frequency
+for stem_token in df:
+    idf[stem_token] = math.log10(N / df[stem_token])
+
+
+# Open for write a file for the document dictionary
+#
+documentfile = open(dirname+'/'+'documents.dat', 'w')
+alldocs.sort()
+for f in alldocs:
+  docindex += 1
+  documentfile.write(f+','+str(docindex)+os.linesep)
+documentfile.close()
+
+#
+# Sort the tokens in the list
+alltokens.sort()
+
+#
+# Define a list for the unique terms  
+g=[]
+
+#
+# Identify unique terms in the corpus
+for i in alltokens:    
+    if i not in g:
+       g.append(i)
+       terms+=1
+
+terms = len(g)
+
+# Output Index to disk file. As part of this process we assign an 'index' number to each unique term.  
+# 
+indexfile = open(dirname+'/'+'index.dat', 'w')
+postfile = open(dirname+'/'+'posting.dat', 'w')
+for i in g:
+  termindex += 1
+  indexfile.write(i+','+str(termindex)+os.linesep)
+  docindex = 0
+  for f in alldocs:
+      docindex += 1
+      key = i + " " + f
+      if key in tf:
+          postfile.write(str(termindex) + "," + str(docindex) + "," + str(tf[key] * idf[i]) + "," + str(tf[key]) + "," + str(df[i])+os.linesep)
+
+postfile.close()
+
+indexfile.close()
+
+
+# Print metrics on corpus
+#
+print ('Start Time: %.2d:%.2d' % (t2.tm_hour, t2.tm_min))
+print ('Indexing Complete, write to disk: %.2d:%.2d' % (t2.tm_hour, t2.tm_min))
+print ("Documents %i" % documents)
+print ("Terms %i" % terms)
+print ("Tokens %i" % tokens)
+print ("Stop Words %i" % len(n_stopwords))
+
+t2 = time.localtime()   
+print ('End Time: %.2d:%.2d' % (t2.tm_hour, t2.tm_min))
+
+
